2bd6461254da226d219ffedf5845f951eac41487
Jacques <jacquesvanluyck@gmail.com>
2025-03-20 12:07:21 +0100

    update and build mock up data

---
+++ src/app/mock-data.ts
@@ -0,0 +1,32 @@
+export let tasks = [
+    {
+        id: 1,
+        task: "Aufräumen",
+        status: "incomplete",
+        checked: false,
+    },
+    {
+        id: 2,
+        task: "Einkaufen",
+        status: "complete",
+        checked: true,
+    },
+    {
+        id: 3,
+        task: "Katze füttern",
+        status: "incomplete",
+        checked: false,
+    },
+    {
+        id: 4,
+        task: "Esel knuddeln",
+        status: "incomplete",
+        checked: false,
+    },
+    {
+        id: 5,
+        task: "Kuchen backen",
+        status: "incomplete",
+        checked: false,
+    }
+]
