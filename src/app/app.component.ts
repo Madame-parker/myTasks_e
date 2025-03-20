855dfc8fa39148e8fb6bef1e6c9d5560c8e0cf95
Jacques <jacquesvanluyck@gmail.com>
2025-03-20 12:04:14 +0100

    update task-list header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { FilterControlsComponent } from "./filter-controls/filter-controls.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { TaskListComponent } from "./task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavigationComponent, FilterControlsComponent, NewTaskComponent],
+  imports: [NavigationComponent, FilterControlsComponent, NewTaskComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
